fa1e9ff372102b85bb1b4c7dbb1f3ad3075d750b
1alberto994 <alby94ct@gmail.com>
2023-02-06 15:03:30 +0100

    creazione di tutte le cartelle più codice

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+    import AppHeader from './components/AppHeader.vue';
+    import AppMain from './components/AppMain.vue';
+    import AppFooter from './components/AppFooter.vue';
+    import axios from "axios";
+    import { store } from './src/store.js';
+    export default {
+      name:"App",
+      components:{
+        AppHeader,
+        AppMain,
+        AppFooter
+      },
+      data() {
+            return {
+                store
+            }
+        },  
+        created() {
+            axios
+                .get(' https://ygoprodeck.com/api-guide/')
+                .then((response) => {
+                    this.store.results = response.data.results;
+                });
+        }
+    };
+
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss" >
+@import "./styles/main.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+import SearchForm from './SearchForm.vue';
+export default {
+    name: 'AppHeader',
+    components: {
+        SearchForm
+    }
+}
+</script>
+
+<template>
+
+    <header>
+
+        <div class="container">
+            <h1 class="">
+               YUGI OH 
+            </h1>
+
+            
+        </div>
+
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
